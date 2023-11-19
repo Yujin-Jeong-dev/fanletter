1a25b89fa910e53f1dabfcdd0da2ce42f043f4a4
정유진 <jane320@naver.com>
2023-11-19 20:16:35 +0900

    편지 디테일 오류 수정 및 주석 제거

--- src/components/LetterList.jsx
+++ src/components/LetterList.jsx
@@ -11,13 +11,10 @@ export default function LetterList() {
     return (
         <>
             <LetterForm />
+            {!filtered.length && <p>{filter === 'All' ? '모두' : filter}에게 남긴 편지가 없습니다. 편지를 작성해주세요! ✏️</p>}
             {letters &&
                 < StyleLetter >
-                    {!filtered.length && <p>{filter === 'All' ? '모두' : filter}에게 남긴 편지가 없습니다. 편지를 작성해주세요! ✏️</p>}
-                    {/* 필터링된 편지들을 원래 Letter 컴포넌트에게 props로 전달해줬음.  */}
-                    {/* 현재는 context로 필터링된 편지를 받아 매핑해줘야하므로 filterLetter함수를 가져온다.  */}
-                    {/* {filtered && filtered.map(letter => <Letter key={letter.id} />)} */}
-                    {filtered.length && <Letter />}
+                    {filtered.length > 0 && <Letter />}
                 </StyleLetter >
             }
         </>
@@ -35,8 +32,5 @@ const StyleLetter = styled.ul`
     cursor:pointer;
 `;
 
-// function filterLetters(letters, filter) {
-//     if (filter === 'All') return letters;
-//     return letters.filter(letter => letter.writedTo === filter);
-// }
+
 
